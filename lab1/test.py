98222272d72f94a9766b008af9971683fa15c40f
FilippaBo <filippa@bonorden.se>
2025-01-25 11:32:15 +0100

    la till isdigit och isalpha. Men fel sak som checkas

--- lab1/test.py
+++ lab1/test.py
@@ -85,16 +85,3 @@ if __name__ == "__main__":
 
 	
 	
-	"""
-	
-	document = ['"They had 16 rolls of duct tape, 2 bags of clothes pins,',
-             '130 hampsters from the cancer labs down the hall, and',
-             'at least 500 pounds of grape jello and unknown amounts of chopped liver"',
-             'said the source on a recent Geraldo interview.']
-tokenize(document)
-['"', 'they', 'had', '16', 'rolls', 'of', 'duct', 'tape', ',', '2', 'bags', 'of', 'clothes', 'pins', ',',
- '130', 'hampsters', 'from', 'the', 'cancer', 'labs', 'down', 'the', 'hall', ',', 'and', 'at', 'least',
- '500', 'pounds', 'of', 'grape', 'jello', 'and', 'unknown', 'amounts', 'of', 'chopped', 'liver', '"', 
- 'said', 'the', 'source', 'on', 'a', 'recent', 'geraldo', 'interview', '.']
- 
- """
--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -8,8 +8,17 @@ def tokenize(lines):
         start = 0
    
         while start < len(line):
-            print(line[start])
-            start = start+1
+           while start.isspace(): #något fel, ska inte vara start.
+                #print(line[start]) <--tidigare kod
+                if start.isdigit():
+                    print(f"{start} is a digit")#ska inte vara start
+                
+                elif start.isalpha():
+                    print(f"{start} is a letter")
+
+                else:
+                    print(f"{start} is a symbol")
+                start = start+1
     return words
 
 wordfreq.tokenize(['apple','pie'])
